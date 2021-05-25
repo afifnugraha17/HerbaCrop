841bfeeeff9d72fce3e12d5b69cc6634468db2ca
yan febriansyah <44943550+febriansya@users.noreply.github.com>
2021-05-25 23:56:58 +0800

    Merge branch 'android' of D:\HerbaCrop with conflicts.

--- app/src/main/java/com/ateam/herbacrop/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/ateam/herbacrop/ui/activity/SplashActivity.kt
@@ -1,26 +1,24 @@
-package com.ateam.herbacrop.ui.activity
+package com.ateam.herbacrop
+ding.inflate(layoutInflater)
+setContentView(splashBinding.root)
+
+Handler(mainLooper).postDelayed({
+    startActivity(Intent(this, MainActivity::class.java))
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.ateam.herbacrop.R
 import com.ateam.herbacrop.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
-    private val splashTimeOut: Long = 5000L
+    private val splashTimeOut: Long = 3000L
     private lateinit var splashBinding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
-        setContentView(splashBinding.root)
-        splashBinding.avi.show()
-
-        Handler(mainLooper).postDelayed({
-            splashBinding.avi.hide()
-            startActivity(Intent(this, HomeActivity::class.java))
+        splashBinding = ActivitySplashBin
             finish()
         }, splashTimeOut)
     }
